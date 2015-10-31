0d02895c3ec47fe7a4a0418c73cd8bbfaa5cda5f
Philipp Gruhn <philipp.gruhn@googlemail.com>
2015-10-31 03:12:42 +0100

    article html manipulation and css embed with experimental css

--- src/main/java/de/gruphi/tdwtf_reader/concurrency/DownloadArticleTask.java
+++ src/main/java/de/gruphi/tdwtf_reader/concurrency/DownloadArticleTask.java
@@ -1,5 +1,8 @@
 package de.gruphi.tdwtf_reader.concurrency;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -18,7 +21,18 @@ public class DownloadArticleTask extends Task<String> {
     protected String call() throws Exception {
         Document doc = Jsoup.connect(article.getUrl().toString()).timeout(30 * 1000).get();
         Elements articleBody = doc.select("div.article-body");
-        return  "<h1>" + article.getTitle() +  "</h1>" + articleBody.toString();
+        String articleHtml = articleBody.toString();
+
+        //show mark's legendary html comments.
+        articleHtml = articleHtml.replaceAll("<\\!--", "<div style='color:red'>");
+        articleHtml = articleHtml.replaceAll("-->", "</div>");
+
+        //embed inline css
+        //TODO that's quite ugly
+        String css =  String.join("", Files.readAllLines(Paths.get("reader.css")));
+
+        return  "<html><body><style>" + css+ "</style><h1>" +
+                article.getTitle() +  "</h1>" + articleHtml + "</html>";
     }
 
 }
