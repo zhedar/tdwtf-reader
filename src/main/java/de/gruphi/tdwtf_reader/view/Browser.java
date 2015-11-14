b9a98c9d2b620d0dfe398649a6e69bf5be5ee4cf
Philipp Gruhn <philipp.gruhn@googlemail.com>
2015-11-14 13:41:49 +0100

    added article information in browser view

--- src/main/java/de/gruphi/tdwtf_reader/view/Browser.java
+++ src/main/java/de/gruphi/tdwtf_reader/view/Browser.java
@@ -1,12 +1,17 @@
 package de.gruphi.tdwtf_reader.view;
+import java.awt.Desktop;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import com.sun.glass.ui.Screen;
 
 import de.gruphi.tdwtf_reader.concurrency.DownloadArticleTask;
 import de.gruphi.tdwtf_reader.db.DataStore;
 import de.gruphi.tdwtf_reader.entities.Article;
+import de.gruphi.tdwtf_reader.entities.Author;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.concurrent.Task;
@@ -16,7 +21,9 @@ import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.geometry.VPos;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
@@ -29,11 +36,17 @@ public class Browser extends Region {
     private final ProgressBar progress = new ProgressBar();
     private final VBox vb;
 
+    private final Hyperlink authorLink = new Hyperlink(),
+                            articleTitleLink = new Hyperlink(),
+                            categoryLink =  new Hyperlink();
+
     private Article currentArticle;
     private BooleanProperty articleReadProp;
+    private ExecutorService executor;
 
     public Browser() throws IOException {
-//        getStyleClass().add("browser");
+        executor = Executors.newCachedThreadPool();
+
         webEngine.getLoadWorker().stateProperty().addListener((ChangeListener<State>) (ov, oldState, newState) -> {
             if (newState == State.SUCCEEDED) {
                 webEngine.executeScript(
@@ -53,12 +66,13 @@ public class Browser extends Region {
             }
         });
 
-        vb = new VBox();
-        vb.setSpacing(2);
+        vb = new VBox(2);
         vb.setAlignment(Pos.CENTER);
         progress.prefWidthProperty().bind(vb.widthProperty());
         progress.visibleProperty().set(Boolean.FALSE);
-        vb.getChildren().addAll(progress, browser);
+        HBox hb = new HBox(5);
+        hb.getChildren().addAll(articleTitleLink, authorLink, categoryLink);
+        vb.getChildren().addAll(hb, progress, browser);
 
         browser.setPrefHeight(Screen.getMainScreen().getHeight());
         getChildren().add(vb);
@@ -80,14 +94,38 @@ public class Browser extends Region {
         task.setOnRunning(e -> progress.visibleProperty().setValue(Boolean.TRUE));
         task.setOnSucceeded(event -> onFinishLoading(event));
         task.setOnFailed(e -> progress.visibleProperty().setValue(Boolean.FALSE));
-        new Thread(task).start();
+        executor.execute(task);
 
         currentArticle = a;
+        setupArticleInfo();
         articleReadProp = prop;
     }
 
     public void onFinishLoading(WorkerStateEvent event) {
         webEngine.loadContent(event.getSource().getValue().toString());
         progress.visibleProperty().setValue(Boolean.FALSE);
+    }
+
+    private void setupArticleInfo() {
+        Author author = currentArticle.getAuthor();
+
+        articleTitleLink.setText(currentArticle.getTitle());
+            articleTitleLink.setOnAction(e -> safeBrowse(currentArticle.getUrl().toString()));
+        authorLink.setText(author.getName());
+            authorLink.setOnAction(e -> safeBrowse(author.getUrl()));
+        categoryLink.setText(currentArticle.getCategory());
+            categoryLink.setOnAction(e -> safeBrowse("http://thedailywtf.com/series/" + currentArticle.getCategory()));
+
+    }
+
+    private void safeBrowse(String url) {
+        try {
+            URI authorURI = new URI(url);
+            Desktop.getDesktop().browse(authorURI);
+        }
+        catch (Exception e) {
+            //TODO handle
+            e.printStackTrace();
+        }
     }
 }
